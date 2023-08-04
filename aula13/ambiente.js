ed857d0ad83f747b902e60ac025134749f5d7c6d
joaofamello <136246095+joaofamello@users.noreply.github.com>
2023-08-03 21:17:49 -0300

    lljlokm

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,11 +1,16 @@
-var c = 1
-while (c <= 6) {
-    console.log(`Passo ${c}`)
-    c++ // c = c + 1
+var c = 1 //inicialização
+while (c <= 6) { //teste lógico
+    console.log(`Passo ${c}`) 
+    c++ // c = c + 1  //incremento
 }
-
 
 do {
     console.log(`Passo ${c}`)
     c++
 } while (c <= 10)
+
+
+//inicialização; teste lógico; incremento.
+for (var c = 1; c<=10; c++) {
+
+}
